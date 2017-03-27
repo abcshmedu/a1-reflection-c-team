0da7a3f81d33250c4866a41f08b6500a917b434a
Michael Eggers <eggers@hm.edu>
2017-03-27 15:21:51 +0200

    first implementation

--- src/main/java/edu/hm/cs/swa/lab01/App.java
+++ src/main/java/edu/hm/cs/swa/lab01/App.java
@@ -6,8 +6,11 @@ package edu.hm.cs.swa.lab01;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IllegalArgumentException, IllegalAccessException
     {
         System.out.println( "Hello World!" );
+        
+        Renderer renderer = new Renderer(new SomeClass(20));
+        System.out.println(renderer.render());
     }
 }
